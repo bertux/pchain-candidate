d4040463b308d751884f30155f497eab61e6ba01
Bertrand Juglas <b@juglas.name>
2019-07-06 17:59:19 +0200

    new variable PCHAIN_LATEST to store latest version number

--- install.sh
+++ install.sh
@@ -1,16 +1,17 @@
 # Updating Ubuntu
 sudo apt-get update && sudo apt-get upgrade -y
 # Installing PCHAIN
-wget https://github.com/pchain-org/pchain/releases/download/v1.0.23/pchain_mainnet_v1.0.23.tar.gz -P ~
+export PCHAIN_LATEST=v1.0.24
+wget https://github.com/pchain-org/pchain/releases/download/$PCHAIN_LATEST/pchain_mainnet_$PCHAIN_LATEST.tar.gz -P ~
 cd ~
-tar -zxf pchain_mainnet_v1.0.23.tar.gz
+tar -zxf pchain_mainnet_$PCHAIN_LATEST.tar.gz
 mkdir -p pchain/log pchain/bin pchain/.pchain pchain/scripts
-cp ~/pchain_mainnet_v1.0.23/pchain ~/pchain/bin/
-cp ~/pchain_mainnet_v1.0.23/run.sh ~/pchain/
+cp ~/pchain_mainnet_$PCHAIN_LATEST/pchain ~/pchain/bin/
+cp ~/pchain_mainnet_$PCHAIN_LATEST/run.sh ~/pchain/
 # Set up an auto-update service
 sudo apt-get install -y jq
 cd ~
-cp ~/pchain_mainnet_v1.0.23/pchain.cron ~/pchain/scripts/
-cp ~/pchain_mainnet_v1.0.23/scripts/* ~/pchain/scripts/
+cp ~/pchain_mainnet_$PCHAIN_LATEST/pchain.cron ~/pchain/scripts/
+cp ~/pchain_mainnet_$PCHAIN_LATEST/scripts/* ~/pchain/scripts/
 sudo crontab -u $USER ~/pchain/scripts/pchain.cron
 crontab -l
